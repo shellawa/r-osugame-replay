cff113886fe42501c3b1a7a3e50dcae9535d6fbb
shellawa <awasymmetry@gmail.com>
2023-06-05 22:27:06 +0700

    formatting

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import os
 import praw
 from dotenv import load_dotenv
+
 load_dotenv()
 
 reddit = praw.Reddit(
-    client_id = os.environ.get("CLIENT_ID"),
-    client_secret = os.environ.get("CLIENT_SECRET"),
-    username = os.environ.get("USERNAME"),
-    password = os.environ.get("PASSWORD"),
-    user_agent = os.environ.get("USER_AGENT")
+    client_id=os.environ.get("CLIENT_ID"),
+    client_secret=os.environ.get("CLIENT_SECRET"),
+    username=os.environ.get("USERNAME"),
+    password=os.environ.get("PASSWORD"),
+    user_agent=os.environ.get("USER_AGENT"),
 )
 
 print("Logged in to", reddit.user.me())
