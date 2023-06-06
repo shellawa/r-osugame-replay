a79e1411e4c0560fd70cce7c60a8e2bdf14aed5f
shellawa <awasymmetry@gmail.com>
2023-06-06 13:49:02 +0700

    replay download

--- utils.py
+++ utils.py
@@ -20,3 +20,15 @@ def get_access_token():  # using lazer access token as it doesn't require user i
         os.environ["EXPIRE_AT"] = str(time() + 86000)
         return res["access_token"]
     return os.environ["BANCHO_ACCESS_TOKEN"]
+
+
+def replay_download(access_token, scoreID):
+    res = requests.get(
+        "https://osu.ppy.sh/api/v2/scores/osu/" + str(scoreID) + "/download",
+        headers={
+            "Accept": "application/octet-stream",
+            "Content-Type": "application/octet-stream",
+            "Authorization": "Bearer " + access_token,
+        },
+    )
+    return res.content
