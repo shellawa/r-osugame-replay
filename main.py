471c9800c690f502f18ffdbc1329b8879aaf1947
shellawa <awasymmetry@gmail.com>
2023-06-06 14:40:32 +0700

    placeholder for the process of rendering score
    scoreIDs are given

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import os
 import praw
+import utils
+import socketio
 from dotenv import load_dotenv
 
 load_dotenv()
+sio = socketio.Client()
+
+
+@sio.event
+def connect():
+    print("ws connection established")
+
+
+sio.connect("https://ordr-ws.issou.best")
+
 
 reddit = praw.Reddit(
     client_id=os.environ["CLIENT_ID"],
@@ -17,4 +29,16 @@ print("Logged in to", reddit.user.me())
 subreddit = reddit.subreddit("allehStestlol")
 
 for submission in subreddit.stream.submissions(skip_existing=True):
-    print(submission.title)
+    print("found new submission:", submission.title)
+    scoreID = int(submission.title)
+    access_token = utils.get_access_token()
+    print("got access token")
+    replay = utils.replay_download(access_token, scoreID)
+    print("got the replay for score", scoreID)
+    renderID = utils.ordr_post(replay)
+    print("posted the replay to o!rdr, renderID:", renderID)
+
+    @sio.on("render_done_json")
+    def done(msg):
+        if msg["renderID"] == renderID:
+            print(msg)
